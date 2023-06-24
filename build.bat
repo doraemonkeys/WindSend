c73f5a75ea6ebceca4b83d0779ca9eab2fedac35
doraemon <doraemonkey@qq.com>
2023-06-24 14:13:56 +0800

    update v0.2.2

--- build.bat
+++ build.bat
@@ -10,6 +10,14 @@ move clipboard-go.exe ../bin/clipboard-go-amd64-windows/clipboard-go.exe
 cd ../flutter/clipboard
 echo %cd%
 
+@REM 输入version
+set /p version=version:
+
+@REM 修改 pubspec.yaml 中的版本号 version:
+set versionStr=version: %version%
+echo %versionStr%
+call sed -i "s/version: .*/%versionStr%/" pubspec.yaml
+
 call flutter build apk --split-per-abi 
 
 if %errorlevel% equ 0 (
