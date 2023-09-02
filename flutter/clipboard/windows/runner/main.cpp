145b6634ecac170bbe22363d2e4c9445ba49f5c6
doraemon <doraemonkey@qq.com>
2023-09-02 17:20:11 +0800

    Modify the window size of the windows client

--- flutter/clipboard/windows/runner/main.cpp
+++ flutter/clipboard/windows/runner/main.cpp
@@ -6,7 +6,7 @@
 #include "utils.h"
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
-                      _In_ wchar_t *command_line, _In_ int show_command) {
+  _In_ wchar_t* command_line, _In_ int show_command) {
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
@@ -20,13 +20,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   flutter::DartProject project(L"data");
 
   std::vector<std::string> command_line_arguments =
-      GetCommandLineArguments();
+    GetCommandLineArguments();
 
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
+  Win32Window::Size size(430, 720);
   if (!window.Create(L"clipboard", origin, size)) {
     return EXIT_FAILURE;
   }
