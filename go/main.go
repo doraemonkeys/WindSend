111b34cd303b34d4620f7e4fc0347497ef44592b
doraemon <doraemonkey@qq.com>
2024-01-22 15:48:29 +0800

    🔖 Upgrade to v1.0.0

--- go/main.go
+++ go/main.go
@@ -12,7 +12,7 @@ var crypter *CbcAESCrypt
 const ProgramName = "WindSend-S-Go"
 const ProgramUrl = "https://github.com/doraemonkeys/WindSend"
 const AboutProgram = `WindSend is a cross-platform clipboard synchronization and file transfer tool, which is easy to use and supports clipboard synchronization and file transfer between different platforms. It is based on the principle of P2P transmission, and the transmission speed is fast and stable. It is a good helper for cross-platform clipboard synchronization and file transfer.`
-const ProgramVersion = "0.6.2"
+const ProgramVersion = "1.0.0"
 
 func init() {
 	InitGlobalLogger()
