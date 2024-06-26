e09c059e8d1578d4e5f51e6586531aa07857e2fb
doraemon <doraemonkey@qq.com>
2024-06-26 18:31:11 +0800

    Release: v1.3.0

--- go/main.go
+++ go/main.go
@@ -12,7 +12,7 @@ var crypter *CbcAESCrypt
 
 const ProgramName = "WindSend-S-Go"
 const ProgramUrl = "https://github.com/doraemonkeys/WindSend"
-const ProgramVersion = "1.2.3"
+const ProgramVersion = "1.3.0"
 
 func init() {
 	InitGlobalLogger()
