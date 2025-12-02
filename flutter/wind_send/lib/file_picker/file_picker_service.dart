fd2cc81fdaa5c3fe8b3f93cb8ba0b260f50c329c
ricky <doraemonkey@qq.com>
2025-12-02 11:05:48 +0800

    fix: fallback to file picker if file picker service is not available

--- flutter/wind_send/lib/file_picker/file_picker_service.dart
+++ flutter/wind_send/lib/file_picker/file_picker_service.dart
@@ -1,11 +1,14 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/services.dart';
+import 'package:wind_send/file_transfer.dart';
 
 class FilePickerService {
   static const platform = MethodChannel('com.doraemon.wind_send/file_picker');
 
   static Future<List<String>> pickFiles(String packageName) async {
-    final List<dynamic> result =
-        await platform.invokeMethod('pickFiles', {'packageName': packageName});
+    final List<dynamic> result = await platform.invokeMethod('pickFiles', {
+      'packageName': packageName,
+    });
     var ret = result
         .cast<String>()
         .map((e) {
@@ -24,8 +27,20 @@ class FilePickerService {
   }
 
   static Future<String> pickFolder(String packageName) async {
-    final String result =
-        await platform.invokeMethod('pickFolder', {'packageName': packageName});
-    return result;
+    try {
+      final String? result = await platform.invokeMethod('pickFolder', {
+        'packageName': packageName,
+      });
+      return result ?? '';
+    } on PlatformException catch (e) {
+      if (e.code == 'UNAVAILABLE') {
+        final result = await FilePicker.platform.getDirectoryPath();
+        if (result == null || result.isEmpty) {
+          throw UserCancelPickException();
+        }
+        return result;
+      }
+      rethrow;
+    }
   }
 }
